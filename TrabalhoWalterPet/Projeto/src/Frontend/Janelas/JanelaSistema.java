f31911b551c9b542407471e28a3b6d631f5ad045
gabriel_leirbag <gabrielcoutomattos@gmail.com>
2026-05-06 23:18:00 -0300

    add(JanelaSistemas): tentiva de fazer os paineis aparecerem

--- TrabalhoWalterPet/Projeto/src/Frontend/Janelas/JanelaSistema.java
+++ TrabalhoWalterPet/Projeto/src/Frontend/Janelas/JanelaSistema.java
@@ -15,12 +15,21 @@ public class JanelaSistema extends JFrame {
 
     public JanelaSistema() {
         setTitle("PetShop Miaujuda");
-        setSize(1080, 720);
+        setSize(1920, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
 
+        PainelEstoque Estoque = new PainelEstoque();
+        PainelProdutos Produtos = new PainelProdutos();
+        PainelRelatorios Relatorios = new PainelRelatorios();
+        PainelVendas Vendas = new PainelVendas();
 
+        setLayout(new GridLayout(2, 2, 10, 10));  // 2 linhas, 2 colunas, espaços entre painéis
+        add(Estoque);
+        add(Produtos);
+        add(Relatorios);
+        add(Vendas);
 
     }
 }
