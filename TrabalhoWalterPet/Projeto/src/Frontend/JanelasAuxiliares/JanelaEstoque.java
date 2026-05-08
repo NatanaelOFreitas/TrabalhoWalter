371b85d435eb464b2f178f8f25ea4fbac104cce0
IanCohen1710 <iancohen9876@gmail.com>
2026-05-08 00:24:08 -0400

    Partiu mimir

--- TrabalhoWalterPet/Projeto/src/Frontend/JanelasAuxiliares/JanelaEstoque.java
+++ TrabalhoWalterPet/Projeto/src/Frontend/JanelasAuxiliares/JanelaEstoque.java
@@ -59,450 +59,183 @@ public class JanelaEstoque extends JFrame {
         JPanel painelCards = new JPanel(new GridLayout(1,3,15,15));
         painelCards.setBackground(fundo);
 
-        lblTotalProdutos = new JLabel("12 Produtos");
+        lblTotalProdutos = new JLabel();
 
-        lblValorTotal = new JLabel("R$ 4.580,00");
+        lblValorTotal = new JLabel();
 
-        lblAlertas = new JLabel("3 Alertas");
+        lblAlertas = new JLabel();
 
         painelCards.add(criarCard("Total de Produtos",lblTotalProdutos));
         painelCards.add(criarCard("Valor Total em Estoque",lblValorTotal));
         painelCards.add(criarCard("Itens com Alerta",lblAlertas));
 
         // Tabela
-        String[] colunas = {"Produto", "Preço", "Quantidade", "Nível"};
+        String[] colunas = {"Produto", "Preço", "Quantidade"};
 
         modeloTabela = new DefaultTableModel(colunas, 0);
 
         tabelaEstoque = new JTable(modeloTabela);
         tabelaEstoque.setRowHeight(30);
+        tabelaEstoque.setFont(new Font("Arial",Font.PLAIN,14));
 
-        tabelaEstoque.setFont(
-                new Font(
-                        "Arial",
-                        Font.PLAIN,
-                        14
-                )
-        );
+        // Exemplos iniciais
+        adicionarProduto("Ração Golden",120.00,10);
+        adicionarProduto("Shampoo Pet", 35.00,3);
+        adicionarProduto("Coleira Azul",20.00,15);
+        adicionarProduto("Brinquedo Bola",18.00, 2);
 
-        // ==================================
-        // DADOS EXEMPLO
-        // ==================================
-        adicionarProduto(
-                "Ração Golden",
-                120.00,
-                10
-        );
+        // Estoque baixo
+        tabelaEstoque.setDefaultRenderer(Object.class,new EstoqueRenderer());
 
-        adicionarProduto(
-                "Shampoo Pet",
-                35.00,
-                3
-        );
+        JScrollPane scrollTabela = new JScrollPane(tabelaEstoque);
 
-        adicionarProduto(
-                "Coleira Azul",
-                20.00,
-                15
-        );
-
-        adicionarProduto(
-                "Brinquedo Bola",
-                18.00,
-                2
-        );
-
-        // ==================================
-        // PROGRESS BAR
-        // ==================================
-        tabelaEstoque
-                .getColumnModel()
-                .getColumn(3)
-                .setCellRenderer(
-                        new ProgressRenderer()
-                );
-
-        // ==================================
-        // ESTOQUE BAIXO VERMELHO
-        // ==================================
-        tabelaEstoque.setDefaultRenderer(
-                Object.class,
-                new EstoqueRenderer()
-        );
-
-        JScrollPane scrollTabela =
-                new JScrollPane(tabelaEstoque);
-
-        // ==================================
-        // AÇÕES
-        // ==================================
-        JPanel painelAcoes =
-                new JPanel();
-
+        // Ações
+        JPanel painelAcoes = new JPanel();
         painelAcoes.setBackground(fundo);
 
-        JButton btnAdicionar =
-                new JButton("Adicionar Quantidade");
-
-        JButton btnRemover =
-                new JButton("Remover Quantidade");
+        JButton btnAdicionar = new JButton("Adicionar Quantidade");
+        JButton btnRemover = new JButton("Remover Quantidade");
 
         estilizarBotao(btnAdicionar);
-
         estilizarBotao(btnRemover);
 
         painelAcoes.add(btnAdicionar);
-
         painelAcoes.add(btnRemover);
 
-        // ==================================
-        // HISTÓRICO
-        // ==================================
-        JPanel painelHistorico =
-                new JPanel(new BorderLayout());
-
+        // Histórico
+        JPanel painelHistorico = new JPanel(new BorderLayout());
         painelHistorico.setBackground(Color.WHITE);
+        painelHistorico.setBorder(BorderFactory.createTitledBorder("Histórico de Movimentações"));
 
-        painelHistorico.setBorder(
-                BorderFactory.createTitledBorder(
-                        "Histórico de Movimentações"
-                )
-        );
+        modeloHistorico = new DefaultListModel<>();
 
-        modeloHistorico =
-                new DefaultListModel<>();
+        JList<String> listaHistorico = new JList<>(modeloHistorico);
 
-        JList<String> listaHistorico =
-                new JList<>(modeloHistorico);
+        painelHistorico.add(new JScrollPane(listaHistorico), BorderLayout.CENTER);
 
-        painelHistorico.add(
-                new JScrollPane(listaHistorico),
-                BorderLayout.CENTER
-        );
-
-        // ==================================
-        // EVENTOS
-        // ==================================
+        // Eventos
         btnAdicionar.addActionListener(e -> {
-
             movimentarEstoque(true);
         });
-
         btnRemover.addActionListener(e -> {
-
             movimentarEstoque(false);
         });
 
-        // ==================================
-        // CENTRO
-        // ==================================
-        JPanel centro =
-                new JPanel(new BorderLayout(15,15));
-
+        // Centro
+        JPanel centro = new JPanel(new BorderLayout(15,15));
         centro.setBackground(fundo);
-
         centro.add(scrollTabela, BorderLayout.CENTER);
-
         centro.add(painelAcoes, BorderLayout.SOUTH);
 
-        // ==================================
-        // ADICIONAR COMPONENTES
-        // ==================================
+        // Adicionar componentes
         painelPrincipal.add(topo, BorderLayout.NORTH);
-
         painelPrincipal.add(painelCards, BorderLayout.CENTER);
-
         painelPrincipal.add(centro, BorderLayout.SOUTH);
-
-        painelPrincipal.add(
-                painelHistorico,
-                BorderLayout.EAST
-        );
+        painelPrincipal.add(painelHistorico,BorderLayout.EAST);
 
         add(painelPrincipal);
+
+        atualizarResumo();
     }
 
-    // ======================================
-    // ADICIONAR PRODUTO
-    // ======================================
-    private void adicionarProduto(
-            String nome,
-            double preco,
-            int quantidade
-    ) {
-
-        modeloTabela.addRow(new Object[]{
-
-                nome,
-                preco,
-                quantidade,
-                quantidade * 10
-        });
+    //Atualizar valores
+    private void atualizarResumo() {
+        int totalProdutos = 0;
+        int alertas = 0;
+        double valorTotal = 0;
+        for(int i = 0;i < modeloTabela.getRowCount();i++) {
+            int quantidade = Integer.parseInt(modeloTabela.getValueAt(i, 2).toString());
+            double preco = Double.parseDouble(modeloTabela.getValueAt(i, 1).toString());
+            totalProdutos += quantidade;
+            valorTotal += preco * quantidade;
+            if(quantidade < 5) {
+                alertas++;
+            }
+        }
+        lblTotalProdutos.setText( totalProdutos + " itens");
+        lblValorTotal.setText(String.format("R$ %.2f",valorTotal));
+        lblAlertas.setText(alertas + " alertas");
     }
 
-    // ======================================
-    // MOVIMENTAÇÃO
-    // ======================================
-    private void movimentarEstoque(
-            boolean adicionar
-    ) {
+    // Adicionar produto
+    private void adicionarProduto(String nome, double preco, int quantidade) {
+        modeloTabela.addRow(new Object[]{nome, preco, quantidade});
+        atualizarResumo();
+    }
 
-        int linha =
-                tabelaEstoque.getSelectedRow();
-
+    // Movimentação
+    private void movimentarEstoque(boolean adicionar) {
+        int linha = tabelaEstoque.getSelectedRow();
         if(linha == -1) {
-
-            JOptionPane.showMessageDialog(
-                    this,
-                    "Selecione um produto!"
-            );
-
+            JOptionPane.showMessageDialog(this, "Selecione um produto!");
             return;
         }
-
-        String valor =
-                JOptionPane.showInputDialog(
-                        this,
-                        "Quantidade:"
-                );
-
-        if(valor == null) return;
-
-        int quantidade =
-                Integer.parseInt(valor);
-
-        int atual =
-                Integer.parseInt(
-                        modeloTabela
-                                .getValueAt(
-                                        linha,
-                                        2
-                                ).toString()
-                );
-
+        String valor = JOptionPane.showInputDialog(this, "Quantidade:");
+        if(valor == null) {
+            return;
+        }
+        int quantidade = Integer.parseInt(valor);
+        int atual = Integer.parseInt(modeloTabela.getValueAt(linha,2).toString());
         int novoValor;
-
         if(adicionar) {
-
             novoValor = atual + quantidade;
-
-        } else {
-
+        }
+        else {
             novoValor = atual - quantidade;
-
             if(novoValor < 0) {
-
-                JOptionPane.showMessageDialog(
-                        this,
-                        "Quantidade inválida!"
-                );
-
+                JOptionPane.showMessageDialog(this, "Quantidade inválida!");
                 return;
             }
         }
+        modeloTabela.setValueAt(novoValor,linha,2);
 
-        modeloTabela.setValueAt(
-                novoValor,
-                linha,
-                2
-        );
 
-        modeloTabela.setValueAt(
-                novoValor * 10,
-                linha,
-                3
-        );
+        String nome =  modeloTabela.getValueAt(linha,0).toString();
 
-        String nome =
-                modeloTabela
-                        .getValueAt(
-                                linha,
-                                0
-                        ).toString();
-
-        modeloHistorico.addElement(
-
-                (adicionar
-                        ? "ENTRADA: "
-                        : "SAÍDA: ")
-
-                        + nome
-
-                        + " ("
-
-                        + quantidade
-
-                        + ")"
-        );
+        modeloHistorico.addElement((adicionar? "ENTRADA: " + nome + " (" + quantidade + ")" :
+                                                "SAÍDA: " + nome + " (" + quantidade + ")"));
+        atualizarResumo();
     }
 
-    // ======================================
-    // CARD
-    // ======================================
-    private JPanel criarCard(
-            String titulo,
-            JLabel valor
-    ) {
-
-        JPanel card =
-                new JPanel(new GridLayout(2,1));
-
+    // Card
+    private JPanel criarCard(String titulo,JLabel valor) {
+        JPanel card = new JPanel(new GridLayout(2,1));
         card.setBackground(Color.WHITE);
-
-        card.setBorder(
-                BorderFactory.createCompoundBorder(
-
-                        BorderFactory.createLineBorder(
-                                roxoPastel,
-                                2
-                        ),
-
-                        BorderFactory.createEmptyBorder(
-                                15,
-                                15,
-                                15,
-                                15
-                        )
-                )
-        );
-
-        JLabel lblTitulo =
-                new JLabel(titulo);
-
-        lblTitulo.setFont(
-                new Font(
-                        "Arial",
-                        Font.BOLD,
-                        18
-                )
-        );
-
+        card.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(roxoPastel,2),
+                                                BorderFactory.createEmptyBorder( 15,15,15,15)));
+        JLabel lblTitulo = new JLabel(titulo);
+        lblTitulo.setFont(new Font("Arial",Font.BOLD,18));
         lblTitulo.setForeground(roxoPastel);
 
-        valor.setFont(
-                new Font(
-                        "Arial",
-                        Font.BOLD,
-                        24
-                )
-        );
+        valor.setFont(new Font("Arial",Font.BOLD,24));
 
         card.add(lblTitulo);
-
         card.add(valor);
-
         return card;
     }
 
-    // ======================================
-    // ESTILO BOTÃO
-    // ======================================
-    private void estilizarBotao(
-            JButton botao
-    ) {
-
+    // Estilo botão
+    private void estilizarBotao(JButton botao) {
         botao.setBackground(roxoPastel);
-
         botao.setForeground(Color.WHITE);
-
         botao.setFocusPainted(false);
-
-        botao.setFont(
-                new Font(
-                        "Arial",
-                        Font.BOLD,
-                        14
-                )
-        );
+        botao.setFont(new Font("Arial",Font.BOLD,14));
     }
 
-    // ======================================
-    // RENDER ESTOQUE BAIXO
-    // ======================================
-    class EstoqueRenderer
-            extends DefaultTableCellRenderer {
-
+    // Render Estoque Baixo
+    class EstoqueRenderer extends DefaultTableCellRenderer {
         @Override
-        public Component getTableCellRendererComponent(
-                JTable table,
-                Object value,
-                boolean isSelected,
-                boolean hasFocus,
-                int row,
-                int column
-        ) {
-
-            Component c =
-                    super.getTableCellRendererComponent(
-                            table,
-                            value,
-                            isSelected,
-                            hasFocus,
-                            row,
-                            column
-                    );
-
-            int quantidade =
-                    Integer.parseInt(
-                            table.getValueAt(
-                                    row,
-                                    2
-                            ).toString()
-                    );
-
+        public Component getTableCellRendererComponent(JTable table,Object value,boolean isSelected,boolean hasFocus,
+                                                       int row,int column) {
+            Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            int quantidade = Integer.parseInt(table.getValueAt(row,2 ).toString());
             if(quantidade < 5) {
-
                 c.setBackground(vermelhoClaro);
-
-            } else {
-
+            }
+            else {
                 c.setBackground(Color.WHITE);
             }
-
             return c;
-        }
-    }
-
-    // ======================================
-    // RENDER PROGRESS BAR
-    // ======================================
-    class ProgressRenderer
-            extends JProgressBar
-            implements javax.swing.table.TableCellRenderer {
-
-        public ProgressRenderer() {
-
-            setMinimum(0);
-
-            setMaximum(100);
-        }
-
-        @Override
-        public Component getTableCellRendererComponent(
-                JTable table,
-                Object value,
-                boolean isSelected,
-                boolean hasFocus,
-                int row,
-                int column
-        ) {
-
-            int quantidade =
-                    Integer.parseInt(
-                            table.getValueAt(
-                                    row,
-                                    2
-                            ).toString()
-                    );
-
-            setValue(
-                    Math.min(
-                            quantidade * 10,
-                            100
-                    )
-            );
-
-            return this;
         }
     }
 }
